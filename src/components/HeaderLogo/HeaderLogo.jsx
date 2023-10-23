85e19d7b4c84a3a0385548dae52c234484c63110
Zeid Bsaibes <zeid.bsaibes@gmail.com>
2023-10-23 21:48:58 +0100

    add 404 back button and lottie animation

--- src/components/HeaderLogo/HeaderLogo.jsx
+++ src/components/HeaderLogo/HeaderLogo.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./HeaderLogo.scss";
 import Logo from "../../assets/logo/BrainFlix-logo.svg";
 import { Link } from "react-router-dom";
-import HomePage from "../../pages/HomePage/HomePage";
 
 function HeaderLogo() {
   return (
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Lottie from "lottie-react";
+import Animation from "../../assets/lottie/lottie-404.json";
+import "./NotFound.scss";
+import { Link } from "react-router-dom";
+import Button from "../../components/Button/Button";
+import HomeIcon from "../../assets/icons/home2.svg";
 
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="lottie">
+      <Lottie
+        animationData={Animation}
+        loop={true}
+        className="lottie__animation"
+      />
+      <h1 className="lottie__title">Page Not Found</h1>
+      <Link to="/" className="lottie__button">
+        <Button text="BACK HOME" />
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
